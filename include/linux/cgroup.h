801bdc4eb27e1141bf02cbb42ac3769857538cd2
Tejun Heo <tj@kernel.org>
2013-11-29 10:42:59 -0500

    cgroup: don't guarantee cgroup.procs is sorted if sane_behavior
    For some reason, tasks and cgroup.procs guarantee that the result is
    sorted.  This is the only reason this whole pidlist logic is necessary
    instead of just iterating through sorted member tasks.  We can't do
    anything about the existing interface but at least ensure that such
    expectation doesn't exist for the new interface so that pidlist logic
    may be removed in the distant future.
    
    This patch scrambles the sort order if sane_behavior so that the
    output is usually not sorted in the new interface.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Li Zefan <lizefan@huawei.com>
    Change-Id: If36e5d70dcafcaaec2bada93104d888421b08889

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -270,6 +270,15 @@ enum {
 	 *
 	 * - Remount is disallowed.
 	 *
+	 * - "tasks" is removed.  Everything should be at process
+	 *   granularity.  Use "cgroup.procs" instead.
+	 *
+	 * - "cgroup.procs" is not sorted.  pids will be unique unless they
+	 *   got recycled inbetween reads.
+	 *
+	 * - "release_agent" and "notify_on_release" are removed.
+	 *   Replacement notification mechanism will be implemented.
+	 *
 	 * - cpuset: tasks will be kept in empty cpusets when hotplug happens
 	 *   and take masks of ancestors with non-empty cpus/mems, instead of
 	 *   being moved to an ancestor.
