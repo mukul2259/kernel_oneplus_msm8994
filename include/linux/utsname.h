59d0a6cae8b9a7769bedff5ad689b58a8a8d867e
Yaroslav Zviezda <acroreiser@gmail.com>
2024-06-16 19:08:58 +0300

    utsname: allow to report arbitrary kernel version to userspace

--- include/linux/utsname.h
+++ include/linux/utsname.h
@@ -69,9 +69,36 @@ static inline void uts_proc_notify(enum uts_proc proc)
 }
 #endif
 
+#ifdef CONFIG_ANDROID_TREBLE_SPOOF_KERNEL_VERSION
+static struct new_utsname utsname_spoofed;
+#endif
 static inline struct new_utsname *utsname(void)
 {
+#ifdef CONFIG_ANDROID_TREBLE_SPOOF_KERNEL_VERSION
+#ifdef CONFIG_ANDROID_TREBLE_BYPASS_KERNEL_VERSION_CHECKS
+	if (!strcmp(current->comm, "system_server") ||
+            !strcmp(current->comm, "zygote") ||
+	    !strcmp(current->comm, "bpfloader") ||
+            !strcmp(current->comm, "perfetto") ||
+            !strcmp(current->comm, "init"))
+	{
+#endif
+		char fake_release_prepended[64];
+
+		strcpy(fake_release_prepended, CONFIG_ANDROID_TREBLE_SPOOF_KERNEL_VERSION_PREFIX);
+	        strcat(fake_release_prepended, "-");
+		strcat(fake_release_prepended, current->nsproxy->uts_ns->name.release);
+		utsname_spoofed = current->nsproxy->uts_ns->name;
+		strcpy(utsname_spoofed.release, fake_release_prepended);
+
+		return &utsname_spoofed;
+#ifdef CONFIG_ANDROID_TREBLE_BYPASS_KERNEL_VERSION_CHECKS
+	} else
+                return &current->nsproxy->uts_ns->name;
+#endif
+#else
 	return &current->nsproxy->uts_ns->name;
+#endif
 }
 
 static inline struct new_utsname *init_utsname(void)
