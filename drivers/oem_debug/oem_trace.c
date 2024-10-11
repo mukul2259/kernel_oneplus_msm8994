34ed2926ef012a5bffb74f9620360fa11e7efc7b
mukul2259 <dhirmukul3@gmail.com>
2024-10-12 00:13:25 +0200

    oem_debug: Remove vmalloc_info traces
     * In accordance with commit def892d812111b03ed10e91aefb004b4b212dac7
     * https://github.com/acroreiser/android_kernel_leeco_msm8976/commit/def892d812111b03ed10e91aefb004b4b212dac7#diff-a0e283743889363ede73525e917266fc9359a56b6e83bae9c96b2579d9bf634fR159

--- drivers/oem_debug/oem_trace.c
+++ drivers/oem_debug/oem_trace.c
@@ -57,14 +57,8 @@
 
 #define IOCTL_OTRACER_PANIC		(1<<12)
 
-struct vmalloc_info {
-	unsigned long	used;
-	unsigned long	largest_chunk;
-};
-
 #ifdef CONFIG_MMU
 #define VMALLOC_TOTAL (VMALLOC_END - VMALLOC_START)
-extern void get_vmalloc_info(struct vmalloc_info *vmi);
 #endif
 
 
@@ -169,7 +163,6 @@ void meminfo_test_saved(void)
 struct sysinfo i;
 	unsigned long committed;
 	unsigned long allowed;
-	struct vmalloc_info vmi;
 	long cached;
 	unsigned long pages[NR_LRU_LISTS];
 	int lru;
@@ -188,8 +181,6 @@ struct sysinfo i;
 			total_swapcache_pages() - i.bufferram;
 	if (cached < 0)
 		cached = 0;
-
-	get_vmalloc_info(&vmi);
 
 	for (lru = LRU_BASE; lru < NR_LRU_LISTS; lru++)
 		pages[lru] = global_page_state(NR_LRU_BASE + lru);
@@ -300,8 +291,8 @@ struct sysinfo i;
 		K(allowed),
 		K(committed),
 		(unsigned long)VMALLOC_TOTAL >> 10,
-		vmi.used >> 10,
-		vmi.largest_chunk >> 10
+		0ul, // used to be vmalloc 'used'
+		0ul  // used to be vmalloc 'largest_chunk'
 #ifdef CONFIG_MEMORY_FAILURE
 		,atomic_long_read(&num_poisoned_pages) << (PAGE_SHIFT - 10)
 #endif
